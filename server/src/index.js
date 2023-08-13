dc4e2cbbfad1f5a290697cf801764c99e280f0ca
Rafael Cena <rafael.cena@hotmail.com>
2023-08-13 10:12:31 -0300

    feat: protegendo as rotas com authorization token

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const taskRoutes = require('./routes/taskRoutes');
+const {authorization} = require('./middlewares/authorization');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+
+app.use(authorization);
 app.use(taskRoutes);
 
 
---
+++ server/src/middlewares/authorization.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv').config();
+
+const token = process.env.TOKEN;
+
+function authorization(req, res, next) {
+  const token = req.headers.authorization;
+  
+  if (!token || token !== `${token}`) {
+    return res.status(401).json({ error: 'Acesso não autorizado' });
+  }
+
+  next();
+}
+
+module.exports = { authorization };
