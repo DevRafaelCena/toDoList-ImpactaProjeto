6cb4d2424c7580171af970baab9b7e8708ff2aaa
Rafael Cena <rafael.cena@hotmail.com>
2023-08-13 10:02:14 -0300

    feat: criando migration e model de task

---
+++ server/src/db/migrations/20230813124505-create-tasks.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      completedAt: {
+        type: Sequelize.DATE
+      },
+      date_created: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  }
+};
