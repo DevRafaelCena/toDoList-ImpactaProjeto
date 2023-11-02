84073358d37f41754fe7253138e85b80dce5cf8c
Rafael Cena <rafael.cena@hotmail.com>
2023-11-02 15:29:37 -0300

    feacture: editar task

--- server/src/controllers/taskController.js
+++ server/src/controllers/taskController.js
@@ -42,6 +42,10 @@ module.exports = {
     try {
       const { id } = req.params;
 
+      if (!id) {
+        return res.status(400).json({ message: 'É necessário informar o id da tarefa.' });
+      }
+
       //verificar se a tarefa existe
       const task = await Task.findByPk(id);
 
@@ -55,6 +59,39 @@ module.exports = {
     } catch (error) {
       console.error('Erro ao deletar tarefa:', error);
       res.status(500).json({ message: 'Erro ao deletar tarefa.' });
+    }
+  },
+
+  updateOneTask: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { title, description } = req.body;
+
+      if (!title || !description) {
+        return res
+          .status(400)
+          .json({ message: 'É necessário informar o título e a descrição da tarefa.' });
+      }
+
+      if (!id) {
+        return res.status(400).json({ message: 'É necessário informar o id da tarefa.' });
+      }
+
+      //verificar se a tarefa existe
+      let task = await Task.findByPk(id);
+
+      if (!task) {
+        return res.status(404).json({ message: 'Tarefa não encontrada.' });
+      }
+
+      await task.update({ title, description });
+
+      task = await Task.findByPk(id);
+
+      res.status(201).json(task);
+    } catch (error) {
+      console.error('Erro ao atualizar tarefa:', error);
+      res.status(500).json({ message: 'Erro ao atualizar tarefa.' });
     }
   },
 };
--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const taskController = require("../controllers/taskController");
+const taskController = require('../controllers/taskController');
 
 // Rota para criar uma nova tarefa
-router.post("/tasks", taskController.createTask);
+router.post('/tasks', taskController.createTask);
 
-router.get("/tasks", taskController.getAllTasks);
+router.get('/tasks', taskController.getAllTasks);
 
-router.delete("/tasks/:id", taskController.deleteOneTask);
+router.delete('/tasks/:id', taskController.deleteOneTask);
+
+router.put('/tasks/:id', taskController.updateOneTask);
 
 module.exports = router;
