ce77d5d3ae92d2fc641630a85f0fae95e63f9403
Rafael Cena <rafael.cena@hotmail.com>
2023-11-02 14:56:58 -0300

    feature: Delete de task

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const taskController = require('../controllers/taskController');
+const taskController = require("../controllers/taskController");
 
 // Rota para criar uma nova tarefa
-router.post('/tasks', taskController.createTask);
+router.post("/tasks", taskController.createTask);
 
-router.get('/tasks', taskController.getAllTasks);
+router.get("/tasks", taskController.getAllTasks);
 
-
+router.delete("/tasks/:id", taskController.deleteOneTask);
 
 module.exports = router;
